dd354e0ddf03e567c25cc78e625876d800e89760
HaiderABB <haider.a.moazzam@gmail.com>
2024-07-09 11:44:18 +0500

    extra logs removed

--- Src/Model/DB/GetFlightsQuery.js
+++ Src/Model/DB/GetFlightsQuery.js
@@ -3,14 +3,11 @@ const Flight_Info = require('../Schemas/flight_info')
 async function getFlights(query, res) {
 
   try {
-    console.log(query);
     const FlightsInfo = await Flight_Info.find({ departure_city: query.DepCity, arrival_city: query.ArrivalCity });
     res.json({
       message: "Flights found",
       data: FlightsInfo
     });
-    // console.log(FlightsInfo);
-
   } catch (err) {
     console.log(err);
     res.status(500).send('');
