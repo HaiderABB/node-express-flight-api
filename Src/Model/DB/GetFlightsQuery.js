598808a11f18cfb60c33a9e0764b6d17a793fbfa
HaiderABB <haider.a.moazzam@gmail.com>
2024-07-09 18:42:29 +0500

    Optimized Flights Query

--- Src/Model/DB/GetFlightsQuery.js
+++ Src/Model/DB/GetFlightsQuery.js
@@ -2,8 +2,21 @@ const Flight_Info = require('../Schemas/flight_info')
 
 async function getFlights(query, res) {
 
+  const DepartureDate = new Date(query.DepDate);
+  const StartOfDay = DepartureDate.setHours(0, 0, 0, 0);
+  const EndOfDay = DepartureDate.setHours(23, 59, 59, 999);
+  console.log(StartOfDay);
+  console.log(EndOfDay);
+
   try {
-    const FlightsInfo = await Flight_Info.find({ departure_city: query.DepCity, arrival_city: query.ArrivalCity });
+    const FlightsInfo = await Flight_Info.find({
+      departure_city: query.DepCity, arrival_city: query.ArrivalCity, departure_date: {
+        $gte: StartOfDay,
+        $lt: EndOfDay
+      }
+    });
+
+
     res.json({
       message: "Flights found",
       data: FlightsInfo
@@ -11,11 +24,7 @@ async function getFlights(query, res) {
   } catch (err) {
     console.log(err);
     res.status(500).send('');
-
   }
 }
-
-
-
 module.exports = getFlights;
 
